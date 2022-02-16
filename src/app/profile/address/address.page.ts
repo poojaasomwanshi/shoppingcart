4b93296e41b299f0628a5bced1edb2e4ced729d4
poojaasomwanshi <74832045+poojaasomwanshi@users.noreply.github.com>
2022-02-16 17:09:41 +0530

    address page

--- src/app/profile/address/address.page.ts
+++ src/app/profile/address/address.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup,FormBuilder,Validators,FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-address',
@@ -6,10 +9,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./address.page.scss'],
 })
 export class AddressPage implements OnInit {
-
-  constructor() { }
+  addressForm:FormGroup;
+  isSubmitted=false;
+  constructor(public formBuilder:FormBuilder, private router: Router) { }
 
   ngOnInit() {
-  }
+    this.addressForm =this.formBuilder.group({
+      housenumber:['',[Validators.required,Validators.minLength(3),Validators.maxLength(15)]],
+      streetadd1:['',[Validators.required,Validators.minLength(3),Validators.maxLength(15)]],
+      streetadd2:['',[Validators.required,Validators.minLength(3),Validators.maxLength(15)]],
+      city:['',[Validators.required,]],
+      country:['',[Validators.required,]],
+      state:['',[Validators.required,]],
+      zipcode:['',[Validators.required]],
 
+        })
+  }
+ 
+
+  submitForm(){
+    this.isSubmitted=true;
+    if(!this.addressForm.valid){
+      console.log("provide required values")
+      return false;
+    }else{
+      alert("address added")
+      console.log(this.addressForm.value)
+      this.addressForm.reset()
+      //move to home page  after login
+      this.router.navigate(['/my-account'])
+
+    }
+
+    
+  }
+  
+
+  
 }
+
+
