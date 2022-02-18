53351b84257b53913043cd33a27f5e749d9c7c01
Kiran201097 <kiransjadhav097@gmail.com>
2022-02-18 17:25:38 +0530

    cart page update

--- src/app/cart/cart/cart.page.ts
+++ src/app/cart/cart/cart.page.ts
@@ -7,15 +7,47 @@ import { CartService } from 'src/app/providers/cart/cart.service';
   styleUrls: ['./cart.page.scss'],
 })
 export class CartPage implements OnInit {
-public productList=[];
+public productList:any=[];
+public isEditEnabled:boolean=false;
 public totalPrice:number=0;
   constructor(private cartsvc:CartService) { }
 
   ngOnInit() {
     this.productList= this.cartsvc.getCart();
+    this.calculateTotalPrice();
+  }
+
+  public edit(){
+    this.isEditEnabled=true;
+  }
+
+  public submit(){
+    this.isEditEnabled=false;
+  }
+
+  public increment(title:string) {
+    this.totalPrice=0;
+    this.productList.forEach(element => {
+      if (title == element.title) {
+          element.quantity++;
+      }
+    });
+    this.calculateTotalPrice();
+  }
+
+  public decrement(title:string) {
+    this.totalPrice=0;
+    this.productList.forEach(element => {
+      if (title == element.title) {
+          element.quantity--;
+      }
+    });
+    this.calculateTotalPrice();
+  }
+
+  public calculateTotalPrice(){
     this.productList.forEach(prod=>{
       this.totalPrice+=prod.quantity*prod.price;
     });
   }
-
 }
